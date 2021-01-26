4b56fec322272f68d66353d075b6506ec626c532
GyanGhoda <gyan.ghoda@outlook.com>
2021-01-25 19:56:41 -0500

    final draft

---
+++ app/src/main/java/com/example/fbla_2021_mobile_final/Camera.java
@@ -0,0 +1,4 @@
+package com.example.fbla_2021_mobile_final;
+
+public class Camera {
+}
---
+++ app/src/main/java/com/example/fbla_2021_mobile_final/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fbla_2021_mobile_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
